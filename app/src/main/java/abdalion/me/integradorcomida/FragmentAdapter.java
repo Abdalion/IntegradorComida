5061f73ed1be92b891333bdf34ad036c778e8ece
Abdalion <egonbush@hotmail.com>
2016-10-14 23:37:04 -0300

    Seccion restaurants finalizada

--- app/src/main/java/abdalion/me/integradorcomida/FragmentAdapter.java
+++ app/src/main/java/abdalion/me/integradorcomida/FragmentAdapter.java
@@ -21,13 +21,13 @@ public class FragmentAdapter extends FragmentStatePagerAdapter {
         listaDeFragments = new ArrayList<>();
 
         listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Astrid y Gaston", R.drawable.astridygaston, "-34.578411, -58.413194"));
-        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("BoraGó", R.drawable.borago, "-31,151"));
-        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Central", R.drawable.central, "-31,151"));
-        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Dom", R.drawable.dom, "-31,151"));
-        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Maido", R.drawable.maido, "-31,151"));
-        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Mani", R.drawable.mani, "-31,151"));
-        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Quintonil", R.drawable.quintonil, "-31,151"));
-        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Tegui", R.drawable.tegui, "-31,151"));
+        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("BoraGó", R.drawable.borago, "-33.404293, -70.598505"));
+        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Central", R.drawable.central, "-12.131663, -77.027850"));
+        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Dom", R.drawable.dom, "-23.565651, -46.667321"));
+        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Maido", R.drawable.maido, "-12.124679, -77.030818"));
+        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Mani", R.drawable.mani, "-23.566733, -46.679247"));
+        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Quintonil", R.drawable.quintonil, "19.431101, -99.191832"));
+        listaDeFragments.add(FragmentLugar.getNewFragmentLugar("Tegui", R.drawable.tegui, "-34.580583, -58.437216"));
 
     }
 
